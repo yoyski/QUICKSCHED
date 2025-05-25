4a9af5da0341b1b29cba0337f9d5a9ece71127fa
yoyski <ferdinandasjr.@gmauil.com>
2025-05-25 11:09:20 +0800

    ok

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/ not include
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'QuickSched',
+        short_name: 'QuickSched',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4f46e5',
+        icons: [
+  {
+    src: 'icons/logo.png',
+    sizes: '192x192',
+    type: 'image/png',
+  },
+  {
+    src: 'icons/logo.png',
+    sizes: '512x512',
+    type: 'image/png',
+  },
+]
+
+      },
+    }),
   ],
   server: {
     port: 5173,
